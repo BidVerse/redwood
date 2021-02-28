69d00a55c48993d6712d96f0d7a8bee6e099fe6f
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-27 18:05:10 -0800

    testing

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,19 @@
-// @ts-ignore
-import { Link, routes } from '@redwoodjs/router'
+import { useQuery } from '@redwoodjs/web'
 
 import Hero from '../../components/Hero'
 import TopBar from '../../components/TopBar'
 import List from '../../components/List'
 
 const HomePage = () => {
+  const get = gql`
+    query getProjects {
+      jobs {
+        name
+      }
+    }
+  `
+  console.log(useQuery(get).data)
+
   return (
     <>
       {Hero()}
